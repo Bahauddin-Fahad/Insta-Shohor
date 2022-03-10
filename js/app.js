f7835e7b4184b8896ddbf642a3d38e3861fdb0c0
Bahauddin-Fahad <bahauddin.fahad819@gmail.com>
2022-03-10 21:15:54 +0600

    4th bug solved

--- js/app.js
+++ js/app.js
@@ -57,6 +57,7 @@ const switchTab = (id) => {
 const createPost = (post) => {
   const userImage = post.userImage;
   const postImage = post.image;
+  const comments = post.comments[0];
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -129,9 +130,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${comments?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${comments?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
